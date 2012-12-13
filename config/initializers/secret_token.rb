7beb4b368cc825ecdc6c5cec15db2810c00a2e9f
Artur Hebda <arturhebda@gmail.com>
2012-12-13 18:58:49 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailwayStation::Application.config.secret_token = '2544a5e6ad241ec124a4bc9545d484aa30cdb62fa4516478a81d6b01933be2001b1c76655f66b5739c38b2119f5a3b20da50802c63d98d5856defa8282b647ee'
