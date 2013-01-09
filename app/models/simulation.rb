44147654dec2f9e05e7be2b3134ffa1bd44d93a2
Artur Hebda <arturhebda@gmail.com>
2013-01-09 21:50:00 +0100

    Change default values of few simulation's parameters. Updated program package - b1eccf04

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -18,22 +18,22 @@ class Simulation < ActiveRecord::Base
     external_delay_info_time_span: { default: 30, values: 1..60 }, # minutes
     go_to_waiting_room_min_time_span: { default: 12, values: 1..60 }, # minutes
     go_to_platform_max_time_span: { default: 5, values: 1..60 }, # minutes
-    info_desk_count: { default: 2, values: 1..20 },
+    info_desk_count: { default: 7, values: 1..20 },
     internal_arrival_time: { default: 5, values: 1..60 },
-    platform_count: { default: 4, values: 1..20 },
+    platform_count: { default: 7, values: 1..20 },
     scheduling_algorithm: { default: nil, values: SCHEDULING_ALGORITHMS.keys },
     waiting_room_capacity: { default: 200, values: 100..2000 }
   }
 
   RANGE_PARAMETERS = {
-    arriving_passenger_count: { default: [10, 80], values: 0..150 },
+    arriving_passenger_count: { default: [10, 70], values: 0..150 },
     coming_time_span_with_ticket: { default: [15, 45], values: 0..60 }, # minutes
     coming_time_span_without_ticket: { default: [15, 50], values: 0..60 }, # minutes
     companion_coming_time_span: { default: [10, 20], values: 0..60 }, # minutes
-    departuring_passenger_count: { default: [10, 80], values: 0..150 },
+    departuring_passenger_count: { default: [10, 70], values: 0..150 },
     external_delay: { default: [5, 60], values: 0..60 }, # minutes
-    selling_ticket_time: { default: [5, 10], values: 0..60 }, # minutes
-    serving_information_time: { default: [5, 15], values: 0..60 } # minutes
+    selling_ticket_time: { default: [2, 5], values: 0..60 }, # minutes
+    serving_information_time: { default: [2, 5], values: 0..60 } # minutes
   }
 
   DISTRIBUTION_PARAMETERS = {
